95c0b1d5e9d2a6017fd887d126338397319e879f
jeejaaa <91847436+jeejaaa@users.noreply.github.com>
2022-06-07 16:06:51 +0200

    ni mom pojecia co robie

--- image_permission.js
+++ image_permission.js
@@ -1,9 +1,9 @@
 const xhr = new XMLHttpRequest();
 const url = 'blob:https://assets.adobe.com/2e8421ec-d02f-409c-bd5e-5d5edd4a6cd7ttps://bar.other/resources/public-data/';
 
-function displayImage() {
+function displayImage(url) {
 	document.getElementById("logo").src = url;
 }
 xhr.open('GET', url);
-xhr.onreadystatechange = displayImage();
+xhr.onreadystatechange = displayImage(url);
 xhr.send();
