c60b1df4711f31b35b4860c62f8033faddf12642
jeejaaa <91847436+jeejaaa@users.noreply.github.com>
2022-06-07 15:38:34 +0200

    ni mom pojecia co robie

--- image_permission.js
+++ image_permission.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest();
-const url = 'https://bar.other/resources/public-data/';
+const url = 'blob:https://assets.adobe.com/2e8421ec-d02f-409c-bd5e-5d5edd4a6cd7ttps://bar.other/resources/public-data/';
 
 xhr.open('GET', url);
 xhr.onreadystatechange = someHandler;
