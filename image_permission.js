e220a28cad2710f9969a14c68591e1f11af30313
jeejaaa <91847436+jeejaaa@users.noreply.github.com>
2022-06-07 16:02:19 +0200

    ni mom pojecia co robie

--- image_permission.js
+++ image_permission.js
@@ -1,6 +1,9 @@
 const xhr = new XMLHttpRequest();
 const url = 'blob:https://assets.adobe.com/2e8421ec-d02f-409c-bd5e-5d5edd4a6cd7ttps://bar.other/resources/public-data/';
 
+function displayImage() {
+	document.getElementById("logo").src = url;
+}
 xhr.open('GET', url);
-xhr.onreadystatechange = someHandler;
+xhr.onreadystatechange = displayImage();
 xhr.send();
